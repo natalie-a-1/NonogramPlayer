93139e938bcb9c26dc06cde3d8c9446b39d38de0
Natalie Hill <nataliehill@Natalies-MacBook-Pro.local>
2022-04-26 14:50:15 -0500

    Added CellGridView methods and javadoc

--- src/edu/ou/cs2334/project5/views/CellGridView.java
+++ src/edu/ou/cs2334/project5/views/CellGridView.java
@@ -4,35 +4,72 @@ import edu.ou.cs2334.project5.models.CellState;
 import javafx.geometry.Pos;
 import javafx.scene.layout.GridPane;
 
-public class CellGridView extends GridPane{
+/**
+ * This class extends GridPane to create its own GridPane of cells.
+ * 
+ * @author Natalie Hill
+ *
+ */
+public class CellGridView extends GridPane {
 
 	private final static String STYLE_CLASS = "nongram-view";
-	private CellView[][]  cellViews;
-	
+	private CellView[][] cellViews;
+
+	/*
+	 * This method creates a CellGridView object.
+	 * 
+	 * @param numRows the number of rows in the CellGrid
+	 * 
+	 * @param numCols the number of columns in the CellGrid
+	 * 
+	 * @param cellLength the length of each cell
+	 */
 	public CellGridView(int numRows, int numCols, int cellLength) {
 		cellViews = new CellView[numRows][numCols];
 		initCells(numRows, numCols, cellLength);
-		
+		this.setAlignment(Pos.CENTER);
+		this.getStylesheets().add(STYLE_CLASS);
+
+	}
+
+	/**
+	 * This initializes each cell in the CellGridView object
+	 * 
+	 * @param numRows    the number of rows in the CellGrid
+	 * @param numCols    the number of columns in the CellGrid
+	 * @param cellLength the length of each cell
+	 */
+	public void initCells(int numRows, int numCols, int cellLength) {
+		this.getChildren().clear();
+
 		for (int row = 0; row < numRows; row++) {
 			for (int col = 0; col < numCols; col++) {
+				CellView tempView = new CellView(cellLength);
+				cellViews[row][col] = tempView;
 				this.add(cellViews[row][col], row, col);
 			}
 		}
-		this.setAlignment(Pos.CENTER);
-		this.getStylesheets().add(STYLE_CLASS);
-		
+
 	}
-	
-	public void initCells(int numRows, int numCols, int cellLength) {
-		this.getChildren().clear();
-		
-	}
-	
+
+	/**
+	 * This method retrieves a CellView at certain index.
+	 * 
+	 * @param rowIdx the row index to be searched
+	 * @param colIdx the column index to be searched
+	 * @return the CellView at the given index
+	 */
 	public CellView getCellView(int rowIdx, int colIdx) {
 		return cellViews[rowIdx][colIdx];
 	}
-	
-	//was CellState in parameter
+
+	/**
+	 * This method sets the state of a given CellView in cellView.
+	 * 
+	 * @param rowIdx the row index to be searched
+	 * @param colIdx the column index to be searched
+	 * @param state  the state of the CellView to be changed to
+	 */
 	public void setCellView(int rowIdx, int colIdx, CellState state) {
 		cellViews[rowIdx][colIdx].setState(state);
 	}
